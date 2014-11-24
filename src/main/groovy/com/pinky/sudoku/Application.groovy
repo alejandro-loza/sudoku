56ceea732f74a93e1114b8b3702a3b3f79d64b28
pinks <pinkymanga>
2014-11-23 21:29:34 -0600

    funcional state

---
+++ src/main/groovy/com/pinky/sudoku/Application.groovy
@@ -0,0 +1,11 @@
+package com.pinky.sudoku
+
+/**
+ * Created by pinks on 23/11/14.
+ */
+class Application {
+    public static void main(String[] args) {
+        Grid grid = new Grid()
+        grid.main()
+    }
+}
